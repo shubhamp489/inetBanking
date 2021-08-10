8d7fbe41510ca89d09104701e2a81f3823692b40
shubham <shubham@shubham>
2021-08-10 07:32:11 +0530

    Shubam -comitted on 10-08-2021

--- src/test/java/com/inetbanking/testcases/BaseClass.java
+++ src/test/java/com/inetbanking/testcases/BaseClass.java
@@ -2,6 +2,7 @@ package com.inetbanking.testcases;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.WebDriver;
@@ -67,6 +68,15 @@ public class BaseClass {
 		FileUtils.copyFile(source, target);
 		System.out.println("Screenshot taken");
 	}
-
+	public String randomestring()
+	{
+		String generatedstring=RandomStringUtils.randomAlphabetic(8);
+		return(generatedstring);
+	}
+	
+	public static String randomeNum() {
+		String generatedString2 = RandomStringUtils.randomNumeric(4);
+		return (generatedString2);
+	}
 	
 }
